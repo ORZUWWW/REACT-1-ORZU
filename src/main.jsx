fd0a49afc6b8c1365768a329dbc989ac4e02d071
ORZU <orzu3008@gmail.com>
2026-03-07 16:09:30 +0500

    orzu

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import page1 from './page1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    
+  </StrictMode>
 )
