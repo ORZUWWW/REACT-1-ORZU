66cca9ccc9c8e520ba985e82eea36189dabf6e46
ORZU <201diviziya201@gmail.com>
2026-03-10 16:37:26 +0500

    Delete src/components directory

--- src/components/Page1img.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import oww from './oww.png'
-const Page1img = () => {
-  return (
-          <img className='w-[668px]' src={oww} alt="" />
-  )
-}
-
-export default Page1img
