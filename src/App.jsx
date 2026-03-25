067a1d533124d1911555a2e223f8e6120c84556a
ORZU <orzu3008@gmail.com>
2026-03-26 00:59:18 +0500

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import './App.css'
 import axios from 'axios'
+import { Route, Router, Routes } from 'react-router'
+import Leaut from './Leaut/Leaut'
+import Home from './page/Home'
+import About from './Page/About'
 const App = () => {
 
-  return (
-    <>
-      
-    
-  
-
-
-    
-    
-    
-    
-    
-    </>
-  )
+  return <Routes>
+  <Route path='/' element={<Leaut />}  >
+ <Route index element={<Home />} />
+ <Route path='/About' element={<About />} />
+  </Route>
+  </Routes>
 }
 
 export default App
