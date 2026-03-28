069975d7669f2c04b02d2e58ec601a4a98565697
ORZU <orzu3008@gmail.com>
2026-03-28 14:04:20 +0500

    aaa

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -6,8 +6,7 @@ const Home = () => {
   let image="http://37.27.29.18:8001/images"
   let api="http://37.27.29.18:8001"
    let [data,setdata]=useState([])  
-  let [name,setname]=useState("")
-  let [idx,setidx]=useState(null)
+
   
 
 async function get() {
@@ -20,194 +19,19 @@ async function get() {
 }
 
 
-// let edit=(e)=>{
-//   setname(e.name)
-//   setidx(e.id)
-// }
-
 
 
 useEffect(()=>{
   get()
 },[])
 
-async function add(obj) {
-    try {
-      await axios.post(url,obj)
-      get()
-    } catch (error) {
-      console.error(error);
-    }
-}
 
 
-// async function editform(e) {
-//    e.preventDefault()
-//    try {
-//     await axios.put(url,{
-//       name:name,
-//       id:idx
-//     })
-//     get()
-//     setname("")
-//     setidx(null)
-//    } catch (error) {
-//     console.error(error);
-//    }
-// }
-
-async function ch(id) {
-  try {
-    await axios.put(`${api}/completed?id=${id}`)
-    get()
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-
-
-async function deleteuser(id) {
-  try {
-    await axios.delete(`${url}?id=${id}`)
-    get()
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-let addform = (e) => {
-  e.preventDefault()
-  let formData = new FormData()
-  formData.append("Images", e.target["inpimage"].files[0])
-  formData.append("Name", e.target["inpname"].value)
-  formData.append("Description", e.target["inpdescription"].value)
-  add(formData)
-  e.target.reset()
-}
 
 
   return (
     <div>
 
-<div className="grid w-[80%] m-auto mt-[20px] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-  {data.map((e, i) => {
-    return (
-      <div
-        key={e.id}
-        className="bg-white shadow-lg rounded-2xl p-5 hover:scale-105 hover:shadow-2xl transition duration-300 border"
-      >
-        <h1 className="text-lg font-semibold text-gray-800 mb-2">
-          {e.name}
-        </h1>
-        <h1 className="text-lg font-semibold text-gray-800 mb-2">
-          {e.isCompleted?"Active":"Inactive"}
-        </h1>
-        <h1 className="text-lg font-semibold text-gray-800 mb-2">
-          {e.description}
-        </h1>
-
-        <p className="text-sm text-gray-500">О
-          ID: {e.id}
-        </p>
-
-        <input onChange={()=>ch(e.id)} type="checkbox" checked={e.isCompleted} />
-
-        <div className='border p-[10px] rounded-2xl'>
-          {
-            e.images.map((img)=>{
-              return <img className='w-[300px]' src={`${image}/${img.imageName}`} alt="" />
-            })
-          }
-        </div>
-
-        <div className="mt-4 gap-1 flex justify-end">
-          <button onClick={()=>deleteuser(e.id)}  className="text-sm bg-red-600 text-white px-3 py-1 rounded-lg hover:bg-blue-600 transition">
-            DELETE
-          </button>
-          <button className="text-sm bg-blue-600 text-white px-3 py-1 rounded-lg hover:bg-blue-600 transition">
-            EDITE
-          </button>
-        </div>
-      </div>
-    );
-  })}
-</div>
-
-<div className='flex mt-[40px] justify-center items-center gap-[200px]'>
-
-<form
-  onSubmit={addform}
-  className="bg-white shadow-lg rounded-2xl p-6 w-[300px] flex flex-col gap-4 border"
->
-  <h2 className="text-xl font-semibold text-gray-800">Add Category</h2>
-
-  <input
-    type="text"
-    name="inpname"
-    placeholder="Enter name..."
-    className="border p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-  />
-  <input
-    type="text"
-    name="inpdescription"
-    placeholder="Enter name..."
-    className="border p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-  />
-  <input
-    type="file"
-    name="inpimage"
-    placeholder="Enter name..."
-    className="border p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-  />
-
-  <button
-    type="submit"
-    className="bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition"
-  >
-    Save
-  </button>
-</form>
-
-
-
-<form
-    // onSubmit={editform}
-    className="bg-yellow-50 shadow-lg rounded-2xl p-6 w-[300px] flex flex-col gap-4 border"
-  >
-    <h2 className="text-xl font-semibold text-gray-800">
-      Edit Category
-    </h2>
-
-    <input
-      type="text"
-      //  value={name}
-      //  onChange={(e) => setname(e.target.value)}
-      className="border p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-400"
-    />
-
-    <div className="flex gap-2">
-      <button
-        type="submit"
-        className="bg-yellow-500 text-white py-2 px-4 rounded-xl hover:bg-yellow-600 transition"
-      >
-        Update
-      </button>
-
-      <button
-        type="button"
-        className="bg-gray-400 text-white py-2 px-4 rounded-xl hover:bg-gray-500"
-      >
-        Cancel
-      </button>
-    </div>
-  </form>
-
-
-
-</div>
 
 
 
