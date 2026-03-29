39c7369906d6b1ebb4c953ae659d3a48b75f4a15
ORZU <orzu3008@gmail.com>
2026-03-29 12:11:29 +0500

    aaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>} >
-
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       </Route>
