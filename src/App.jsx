7b799cb5bca9108180fd784ec0cdfef05e7b4fca
ORZU <orzu3008@gmail.com>
2026-04-02 22:11:45 +0500

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router";
-import About from "./page/About";
+import { Routes, Route } from "react-router-dom";
+import About from "./Page/About";
 import Layout from "./Leaut/Leaut";
-import Counter from "./page/Home";
+import Counter from "./Page/Home";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout/>} >
-      <Route path="/" element={<Counter />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Counter />} />
+        <Route path="about" element={<About />} />
       </Route>
     </Routes>
   );
